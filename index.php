a84fe9120d182dd7ef1bf90234b4fd409be0bd6f
Justtoconfirm <office@justtoconfirm.co.uk>
2018-01-16 21:11:14 +0000

    Error message placeholders added

--- index.php
+++ index.php
@@ -3,10 +3,18 @@
 session_start();
 
 // Input field values are empty unless user enters a value
+$firstnameInput = '';
+$lastnameInput = '';
 $emailInput = '';
+$usernameInput = '';
+$passwordInput = '';
 
 // Error messages - empty by default
+$firstnameErr = '';
+$lastnameErr = '';
 $emailErr = '';
+$usernameErr = '';
+$passwordErr = '';
 
 // Click 'Register' button
 if (isset($_POST['Register'])) {
@@ -38,18 +46,30 @@ if (isset($_POST['Register'])) {
 
 <form action="" method="post">
 	<label for="first_name">First name:</label><br/>
-	<input type="text" name="first_name" id="first_name" /><br/>
+	<input type="text" name="first_name" id="first_name" value="<?php echo $firstnameInput; ?>" /><br/>
+	<!-- Error message -->
+	<?php echo $firstnameErr; ?><br/>
+	<!-- / Error message -->
 	<label for="last_name">Last name:</label><br/>
-	<input type="text" name="last_name" id="last_name" /><br/>
+	<input type="text" name="last_name" id="last_name" value="<?php echo $lastnameInput; ?>" /><br/>
+	<!-- Error message -->
+	<?php echo $lastnameErr; ?><br/>
+	<!-- / Error message -->
 	<label for="e_mail">Email:</label><br/>
 	<input type="text" name="e_mail" id="e_mail" value="<?php echo $emailInput; ?>" /><br/>
 	<!-- Error message -->
 	<?php echo $emailErr; ?><br/>
 	<!-- / Error message -->
 	<label for="user_name">Username:</label><br/>
-	<input type="text" name="user_name" id="user_name" /><br/>
+	<input type="text" name="user_name" id="user_name" value="<?php echo $usernameInput; ?>" /><br/>
+	<!-- Error message -->
+	<?php echo $usernameErr; ?><br/>
+	<!-- / Error message -->
 	<label for="pass_word">Password:</label><br/>
-	<input type="password" name="pass_word" id="pass_word" /><br/>
+	<input type="password" name="pass_word" id="pass_word" value="<?php echo $passwordInput; ?>" /><br/>
+	<!-- Error message -->
+	<?php echo $passwordErr; ?><br/>
+	<!-- / Error message -->
 	<br/><button type="submit" name="Register">Create account</button>
 </form>
 
