650b426bad5d7cf6aafc97b9ba9702a32e39739d
Justtoconfirm <office@justtoconfirm.co.uk>
2018-04-30 20:16:44 +0100

    The read.php file has been added

---
+++ read.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>PHP Project - Read</title>
+
+<style>
+label {
+	display: block;
+	margin: 5px 0;
+}
+</style>
+
+</head>
+<body>
+
+<form method="post">
+	<label for="location">Location</label>
+	<input type="text" name="location" id="location" />
+	<input type="submit" name="submit" value="Submit" />
+</form>
+
+<a href="index.php">Back to home</a>
+
+</body>
+</html>
